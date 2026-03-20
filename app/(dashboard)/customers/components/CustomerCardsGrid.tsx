0a21512fe98e4b9659de47046cc5365747cc948c
Rajguru-Aditya <aditya.rajguru@ibridgellc.com>
2026-03-20 23:45:12 +0530

    Debugging

--- app/(dashboard)/customers/components/CustomerCardsGrid.tsx
+++ app/(dashboard)/customers/components/CustomerCardsGrid.tsx
@@ -4,6 +4,7 @@ import RetentionCard from './RetentionCard'
 import ActivityCard from './ActivityCard'
 
 const CustomerCardsGrid = ({ data}: { data: any }) => {
+  console.log("Data customer: ", data)
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <GrowthCard data={data.growth} />
