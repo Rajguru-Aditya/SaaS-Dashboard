3cfebf4e0fc43e03060c3f84a0b898dc9e89038a
Rajguru-Aditya <aditya.rajguru@ibridgellc.com>
2026-03-21 23:57:56 +0530

    Fix server issue

--- lib/services/customerGrowthServices.ts
+++ lib/services/customerGrowthServices.ts
@@ -1,9 +1,11 @@
 export const getCustomerGrowthData = async () => {
     try {
+        console.log("BASE URL:", process.env.NEXT_PUBLIC_BASE_URL)
         const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/customers`);
         if (!response.ok) {
             throw new Error('Failed to fetch customer growth data');
         }
+        console.log("Response", response)
         const data = await response.json();
         return data.data;
     } catch (error) {
